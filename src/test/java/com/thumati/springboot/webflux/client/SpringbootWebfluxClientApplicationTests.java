a253b644319bcfc79d1b7f5fa6b674bb4d192a01
vthumati <vasanthakumar.thumati@davita.com>
2019-09-03 13:58:05 -0500

    initial commit

---
+++ src/test/java/com/thumati/springboot/webflux/client/SpringbootWebfluxClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thumati.springboot.webflux.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWebfluxClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
